8b97630732585cf2e4707d5f19b6785792dd23b1
Amelia Henderson <ahenderson91@gmail.com>
2014-04-05 18:32:42 -0400

    Tracking code

--- 1_UI_Layer/pages/index.php
+++ 1_UI_Layer/pages/index.php
@@ -8,7 +8,18 @@
 		<link href="/Utilities/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<link href="/1_UI_Layer/css/cover.css" rel="stylesheet">
 		<link href="/1_UI_Layer/css/helpkonnect.css" rel="stylesheet">
-		<link href="/1_UI_Layer/css/signin.css" rel="stylesheet">	
+		<link href="/1_UI_Layer/css/signin.css" rel="stylesheet">
+        
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-49692941-1', 'helpkonnect.com');
+            ga('send', 'pageview');
+
+        </script>
 
 	</head>
 	<body>
